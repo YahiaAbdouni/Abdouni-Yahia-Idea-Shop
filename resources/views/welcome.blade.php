cf48075e76dee1bb118a6b42f43875b5e6731283
Abdouni Yahia <yabdouni3@gmail.com>
2020-08-24 18:25:25 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,9 +67,9 @@
   
                   <h6 class="sidebar-title">Categories</h6>
                   <div class="row link-color-default fs-14 lh-24">
-                    @foreach ($categories as $category)
+                    {{-- @foreach ($categories as $category)
                     <div class="col-6"><a href="{{route('category.show', $category->id)}}">{{$category->name}}</a></div>
-                    @endforeach
+                    @endforeach --}}
                   </div>
   
                   <hr>
