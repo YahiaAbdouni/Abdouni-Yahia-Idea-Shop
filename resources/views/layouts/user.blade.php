3815885c733561d7766a8bd98f0ca6f2360eaa0a
Abdouni Yahia <yabdouni3@gmail.com>
2020-09-03 22:53:09 +0100

    v 1.0

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -113,6 +113,7 @@
                 <a class="social-facebook" href="https://www.facebook.com/yabdouni3"><i class="fa fa-facebook"></i></a>
                 <a class="social-twitter" href="https://twitter.com/yabdouni3"><i class="fa fa-twitter"></i></a>
                 <a class="social-instagram" href="https://www.instagram.com/abdouni_yahia/"><i class="fa fa-instagram"></i></a>
+                <a class="social-dribbble" href="https://dribbble.com/thethemeio"><i class="fa fa-dribbble"></i></a>
               </div>
             </div>
   
